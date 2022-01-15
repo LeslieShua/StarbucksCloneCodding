34981e9d720d9d37b785358c3d3ff4346b86d3b7
[LesliShua] <[southenmeat@gmail.com]>
2022-01-15 23:34:06 +0900

    공통 모듈 분리

--- js/java.js
+++ js/java.js
@@ -1,22 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl =  searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function() {
-  //Losic..
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function() {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function() {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
-
 // 화면 맨오른쪽 뱃지부분 
 
 const badgeEl = document.querySelector('header .badges');
@@ -179,6 +160,3 @@ spyEls.forEach(function(spyEl) {
 /* {} <-- 객체 데이터*/
 
 
-
-const thisYear = document.querySelector('.this-year');  //<--getFullYear
-thisYear.textContent = new Date().getFullYear();  //2021
